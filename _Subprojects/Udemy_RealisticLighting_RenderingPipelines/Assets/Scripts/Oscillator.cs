ea80f2705e421c8e953d54f6e4e70e5da75a9e44
Marco Caruso <marco.caruso@dtales.it>
2022-09-02 09:23:32 +0200

    Update

---
+++ _Subprojects/Udemy_RealisticLighting_RenderingPipelines/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] private Vector3 directionMultiplier;
+    [SerializeField] private float frequencyMultiplier = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xOffset = directionMultiplier.x * Mathf.Sin(frequencyMultiplier * Time.time);
+        float yOffset = directionMultiplier.y * Mathf.Sin(frequencyMultiplier * Time.time);
+        float zOffset = directionMultiplier.z * Mathf.Sin(frequencyMultiplier * Time.time);
+        transform.position += new Vector3(xOffset, yOffset, zOffset);
+
+    }
+}
